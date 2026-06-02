a3ceb3309728d497a838df8d69c28e27db380d08
Sanwarmalff <sewalsanwarmal@gmail.com>
2026-06-02 15:04:05 +0530

    Add AdminReceiver class for device administration

---
+++ app/src/main/java/com/donotdisturb/app/AdminReceiver.kt
@@ -0,0 +1,7 @@
+package com.donotdisturb.app
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class AdminReceiver : DeviceAdminReceiver()
